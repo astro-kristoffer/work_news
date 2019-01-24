56f4e64918bfc1ba667ce46910043609de167bdf
astro-kristoffer <krist.sycheva@gmail.com>
2019-01-24 18:09:44 +0300

    Nothing new

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
-Admin.create!(
+admin_first = Admin.new(
   :email => 'krist.sycheva@gmail.com',
   :password => '123456'
 )
-Admin.create!(
+admin_first.save
+
+admin_second = Admin.new(
   :email => 'krist.sycheva@mail.ru',
   :password => '123456'
 )
+admin_second.save
